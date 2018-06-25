66a4d8f926303a27949be51216c31f9e7a99732b
Anudeep <1676567+anudeeptreddy@users.noreply.github.com>
2018-06-24 21:13:54 -0400

    verifying a permutation using a dictionary

--- Ch1_Arrays_And_String/02_is_a_permutation.py
+++ Ch1_Arrays_And_String/02_is_a_permutation.py
@@ -1,10 +1,23 @@
 # Algorithm to check if one string is a permutation of the other
 
+counter = {}
+
 
 def is_permutation(str1, str2):
-    str1_sorted = sorted(str1)
-    str2_sorted = sorted(str2)
-    return True if str1_sorted == str2_sorted else False
+    for ch in str1:
+        if ch in counter.keys():
+            counter[ch] += 1
+            continue
+        counter[ch] = 1
+
+    for ch in str2:
+        if ch not in counter.keys():
+            return False
+        counter[ch] -= 1
+        if counter[ch] == 0:
+            del counter[ch]
+
+    return not counter
 
 
 if __name__ == '__main__':
